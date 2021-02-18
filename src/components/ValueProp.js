043c645a3af46260e7958aa6208acd194b13534f
Marco Burro <marcoburro98@gmail.com>
2021-02-18 11:43:37 +0100

    br

--- src/components/ValueProp.js
+++ src/components/ValueProp.js
@@ -8,7 +8,7 @@ const ValueProp = props => {
       <div className="value-prop-content">
         <div className="main-message">
           <h1>McSoftware S.p.a.</h1>
-          <p className="main-subtitle">Applicazioni Web, Gestionali, Piattaforme di Analisi Dati</p>
+          <p className="main-subtitle">Applicazioni Web<br/>Gestionali<br/>Piattaforme di Analisi Dati</p>
           <Button label="Richiedi un preventivo" />
         </div>
         <div className="main-photo"></div>
